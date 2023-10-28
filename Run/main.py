de89dcf0c08ef5003eb82a10e933ec3d9a90f2f0
Remus <andreibujor2003@gmail.com>
2023-10-28 22:26:00 +0300

    grounding dino inference implementation

--- Run/main.py
+++ Run/main.py
@@ -7,7 +7,9 @@ sys.path.append(os.getcwd())
 sys.path.append('..')
 
 from Preprocessors.Preprocess import Preprocess
-from CLIP.Inference import Inference
+from Preprocessors.FolderTransformer import FolderTransformer
+#from CLIP.Inference import Inference
+from grounding_dino.inference import DinoInference
 from utils.utils import configure_logger
 
 
@@ -18,14 +20,24 @@ def main(cfg) -> None:
     :param cfg:
     :return:
     """
-    logger = configure_logger(cfg.CLIP.logger_name, cfg.CLIP.file_handler)
+#    logger = configure_logger(cfg.CLIP.logger_name, cfg.CLIP.file_handler)
 
-    preprocessor = Preprocess(cfg.CLIP.path_to_folder, cfg.CLIP.csv_column, cfg.CLIP.csv_name, logger)
-    preprocessor()
+    # preprocessor = Preprocess(cfg.CLIP.path_to_folder, cfg.CLIP.csv_column, cfg.CLIP.csv_name, logger)
+    # reprocessor()
 
-    inferencer = Inference(cfg.CLIP.output_folder, cfg.CLIP.model_type, cfg.CLIP.labels, cfg.CLIP.csv_name, cfg.CLIP.csv_column, cfg.CLIP.device, logger)
-    inferencer()
 
+    # inferencer = Inference(cfg.CLIP.output_folder, cfg.CLIP.model_type, cfg.CLIP.labels, cfg.CLIP.csv_name, cfg.CLIP.csv_column, cfg.CLIP.device, logger)
+    # inferencer()
+
+    preprocess_dino = FolderTransformer(cfg.DINO.path_to_folder, cfg.DINO.csv_column, cfg.DINO.csv_name)
+    preprocess_dino()
+
+    inferencer_dino = DinoInference(cfg.DINO.model_config_path, cfg.DINO.model_checkpoint_path, cfg.DINO.label_file, cfg.DINO.csv_name, cfg.DINO.csv_column, cfg.DINO.logger_name)
+    inferencer_dino(float(cfg.DINO.box_threshold), float(cfg.DINO.text_threshold))
+     
+    
+
+    
 
 if __name__ == '__main__':
     main()
---
+++ grounding_dino/inference.py
@@ -0,0 +1,85 @@
+from typing import Any
+from groundingdino.util.inference import load_image, load_model, predict, annotate
+
+import pandas as pd
+import os
+import sys
+
+sys.path.append(os.getcwd())
+sys.path.append('..')
+
+
+from Preprocessors.CsvPreprocessor import CsvPreprocessor
+from Preprocessors.FolderTransformer import FolderTransformer
+from utils import utils
+
+class DinoInference:
+    def __init__(self, model_config_path, model_checkpoint_path, label_file, csv_file, image_column, logger):
+        self.__model_config_path = model_config_path
+        self.__model_checkpoint_path = model_checkpoint_path
+        self.__label_file = label_file
+        self.__csv_file = csv_file
+        self.__image_column = image_column
+        self.__logger_name = logger
+        self.__model = None
+        self.__prompt = None
+        self.__csv_preprocessor = CsvPreprocessor(self.__csv_file, image_column)
+        self.__boxes = None
+        self.__logits = None
+        self.__phrases = None
+
+    
+    def load_model(self):
+        self.__model = load_model(self.__model_config_path, self.__model_checkpoint_path)
+
+    def load_labels(self):
+        self.__txt_labels = utils.get_labels(self.__label_file)
+
+        print(self.__txt_labels)
+        self.__prompt = '. '.join(self.__txt_labels)
+        self.__prompt += '.'
+
+        print(self.__prompt)
+    
+    def run_inference(self, image_path, BOX_THRESHOLD, TEXT_THRESHOLD):
+
+        image_source, image = load_image(image_path=image_path)
+
+        self.__boxes, self.__logits,  self.__phrases = predict(
+            model=self.__model,
+            image=image,
+            caption=self.__prompt,
+            box_threshold=BOX_THRESHOLD,
+            text_threshold=TEXT_THRESHOLD)
+
+    
+    def run(self, BOX_THRESHOLD, TEXT_THRESHOLD):   
+        self.__logger.info('Reading image labels....')
+        images = self.__csv_preprocessor()
+        self.load_labels()
+        self.__df = pd.DataFrame()
+        list_phrases = []
+        list_boxes = []
+        list_images = []
+        for image in images:
+            self.__logger.info('Running inference on image ' + image)
+
+            self.run_inference(image, BOX_THRESHOLD=BOX_THRESHOLD, TEXT_THRESHOLD=TEXT_THRESHOLD)
+
+            self.__logger.info('Image got boxes ' + str(self.__phrases))
+
+            list_images.append(image)
+            list_phrases.append(str(self.__phrases))
+            list_boxes.append(str(self.__boxes.cpu().detach().numpy()))
+
+        self.__df.insert(0, "Image_path", list_images, True)
+        self.__df.insert(1, "Labels", list_phrases, True)
+        self.__df.insert(2, "Boxes", list_boxes, True)
+
+        self.__df.to_csv('b.csv', index=False)
+
+    def __call__(self, BOX_THRESHOLD, TEXT_THRESHOLD):
+        self.__logger = utils.configure_logger(self.__logger_name, str(self.__logger_name) + '.log')
+        self.load_model()
+        self.run(BOX_THRESHOLD=BOX_THRESHOLD, TEXT_THRESHOLD=TEXT_THRESHOLD)
+
